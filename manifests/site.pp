3f604b2de7c2f178ae98fbf9a148462948a8528c
petems <p.morsou@gmail.com>
2013-03-12 23:37:39 +0000

    Added growl fork

--- manifests/site.pp
+++ manifests/site.pp
@@ -70,7 +70,6 @@ node default {
   include ruby::1_8_7
   include ruby::1_9_2
   include ruby::1_9_3
-  include ruby::2_0_0
 
   # common, useful packages
   package {
@@ -92,5 +91,6 @@ node default {
   include rabbitmq
   include propane
   include sublime_text_2
+  include growl_fork
 
 }
