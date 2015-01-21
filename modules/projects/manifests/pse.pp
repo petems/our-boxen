a8e720653fe819137b4eefe13b7bc09c0638a967
Peter Souter <peter.souter@puppetlabs.com>
2015-01-21 12:47:53 +0000

    Adding new PSE project with the base stuff

---
+++ modules/projects/manifests/pse.pp
@@ -0,0 +1,20 @@
+# Base things all PSE's need! :)
+class projects::pse {
+  class { 'vagrant':
+    version => '1.6.5'
+  }
+  class { 'virtualbox':
+    version => '4.3.14',
+    patch_level => '95030'
+  }
+  class { 'vmware_fusion':}
+  class { 'hipchat':}
+  class { 'caffeine':}
+  class { 'smcfancontrol':}
+  class { 'viscosity':}
+
+  if ($::memorysize_mb < '8192') {
+    notify{"As a PSE, you'll probably need more than 8GB of Memory":}
+    notify{"Open a support request to get 16GB ASAP!":}
+  }
+}
